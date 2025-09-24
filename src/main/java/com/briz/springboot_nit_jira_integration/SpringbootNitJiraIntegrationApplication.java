9f79c4ea0485d4a9f973797eca1e96dba946f940
kanchan raju <kanchanraju2015@gmail.com>
2025-09-24 09:22:26 +0530

    hello cimmit

---
+++ src/main/java/com/briz/springboot_nit_jira_integration/SpringbootNitJiraIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_nit_jira_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNitJiraIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNitJiraIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/briz/springboot_nit_jira_integration/SpringbootNitJiraIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_nit_jira_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNitJiraIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
